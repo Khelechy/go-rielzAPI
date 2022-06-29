1219886c308b49f212b7b9b2275beefab2571c35
khelechy <onyekwerekelechimac@gmail.com>
2022-06-30 00:18:16 +0100

    added port

--- api/controllers/base.go
+++ api/controllers/base.go
@@ -2,44 +2,45 @@
 package controllers
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 
-    "github.com/gorilla/mux"
-    "github.com/jinzhu/gorm"
-    _ "github.com/jinzhu/gorm/dialects/postgres" //postgres
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres" //postgres
 
-    _ "github.com/khelechy/rielzapi/api/models/docs" 
-    httpSwagger "github.com/swaggo/http-swagger"
+	_ "github.com/khelechy/rielzapi/api/models/docs"
+	httpSwagger "github.com/swaggo/http-swagger"
 
-    "github.com/khelechy/rielzapi/api/middlewares"
+	"github.com/khelechy/rielzapi/api/middlewares"
 	"github.com/khelechy/rielzapi/api/models"
-    "github.com/khelechy/rielzapi/api/responses"
+	"github.com/khelechy/rielzapi/api/responses"
 )
 
 type App struct {
-    Router *mux.Router
-    DB     *gorm.DB
+	Router *mux.Router
+	DB     *gorm.DB
 }
 
 // Initialize connect to the database and wire up routes
 func (a *App) Initialize(DbHost, DbPort, DbUser, DbName, DbPassword string) {
-    var err error
-    DBURI := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", DbHost, DbPort, DbUser, DbName, DbPassword)
+	var err error
+	DBURI := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", DbHost, DbPort, DbUser, DbName, DbPassword)
 
-    a.DB, err = gorm.Open("postgres", DBURI)
-    if err != nil {
-        fmt.Printf("\n Cannot connect to database %s", DbName)
-        log.Fatal("This is the error:", err)
-    } else {
-        fmt.Printf("We are connected to the database %s", DbName)
-    }
+	a.DB, err = gorm.Open("postgres", DBURI)
+	if err != nil {
+		fmt.Printf("\n Cannot connect to database %s", DbName)
+		log.Fatal("This is the error:", err)
+	} else {
+		fmt.Printf("We are connected to the database %s", DbName)
+	}
 
-    a.DB.Debug().AutoMigrate(&models.User{}, &models.House{}, &models.Tenant{}) //database migration
+	a.DB.Debug().AutoMigrate(&models.User{}, &models.House{}, &models.Tenant{}) //database migration
 
-    a.Router = mux.NewRouter().StrictSlash(true)
-    a.initializeRoutes()
+	a.Router = mux.NewRouter().StrictSlash(true)
+	a.initializeRoutes()
 }
 
 // @title Rielz API
@@ -53,37 +54,35 @@ func (a *App) Initialize(DbHost, DbPort, DbUser, DbName, DbPassword string) {
 // @host localhost:5000
 // @BasePath /
 func (a *App) initializeRoutes() {
-    a.Router.Use(middlewares.SetContentTypeMiddleware) // setting content-type to json
-    a.Router.PathPrefix("/swagger").Handler(httpSwagger.WrapHandler)
-    a.Router.HandleFunc("/", home).Methods("GET")
-    a.Router.HandleFunc("/register", a.UserSignUp).Methods("POST")
-    a.Router.HandleFunc("/login", a.Login).Methods("POST")
+	a.Router.Use(middlewares.SetContentTypeMiddleware) // setting content-type to json
+	a.Router.PathPrefix("/swagger").Handler(httpSwagger.WrapHandler)
+	a.Router.HandleFunc("/", home).Methods("GET")
+	a.Router.HandleFunc("/register", a.UserSignUp).Methods("POST")
+	a.Router.HandleFunc("/login", a.Login).Methods("POST")
 	a.Router.HandleFunc("/api/houses", a.GetHouses).Methods("GET")
 	a.Router.HandleFunc("/api/houses/{id:[0-9]+}", a.GetHouseById).Methods("GET")
-    a.Router.HandleFunc("/api/users", a.GetUsers).Methods("GET")
-    a.Router.HandleFunc("/api/users/{id:[0-9]+}", a.GetUserById).Methods("GET")
-    a.Router.HandleFunc("/api/houses/landlord/{id:[0-9]+}", a.GetHousesByLandlordId).Methods("GET")
-    a.Router.HandleFunc("/api/houses/{state}", a.GetHousesByState).Methods("GET")
-	
+	a.Router.HandleFunc("/api/users", a.GetUsers).Methods("GET")
+	a.Router.HandleFunc("/api/users/{id:[0-9]+}", a.GetUserById).Methods("GET")
+	a.Router.HandleFunc("/api/houses/landlord/{id:[0-9]+}", a.GetHousesByLandlordId).Methods("GET")
+	a.Router.HandleFunc("/api/houses/{state}", a.GetHousesByState).Methods("GET")
 
 	s := a.Router.PathPrefix("/api").Subrouter() // routes that require authentication
-    s.Use(middlewares.AuthJwtVerify)
+	s.Use(middlewares.AuthJwtVerify)
 
-
-    s.HandleFunc("/houses", a.CreateHouse).Methods("POST")
-    s.HandleFunc("/houses/tenant", a.AddTenant).Methods("POST")
-    s.HandleFunc("/houses/landlord/", a.GetHousesByLandlord).Methods("GET")
-    s.HandleFunc("/api/users/{id:[0-9]+}", a.GetUserById).Methods("GET")
-    s.HandleFunc("/users/{id:[0-9]+}", a.UpdateUser).Methods("PUT")
-    s.HandleFunc("/houses/{id:[0-9]+}", a.UpdateHouse).Methods("PUT")
-    s.HandleFunc("/houses/{id:[0-9]+}", a.DeleteHouse).Methods("DELETE")
+	s.HandleFunc("/houses", a.CreateHouse).Methods("POST")
+	s.HandleFunc("/houses/tenant", a.AddTenant).Methods("POST")
+	s.HandleFunc("/houses/landlord/", a.GetHousesByLandlord).Methods("GET")
+	s.HandleFunc("/api/users/{id:[0-9]+}", a.GetUserById).Methods("GET")
+	s.HandleFunc("/users/{id:[0-9]+}", a.UpdateUser).Methods("PUT")
+	s.HandleFunc("/houses/{id:[0-9]+}", a.UpdateHouse).Methods("PUT")
+	s.HandleFunc("/houses/{id:[0-9]+}", a.DeleteHouse).Methods("DELETE")
 }
 
 func (a *App) RunServer() {
-    log.Printf("\nServer starting on port 5000")
-    log.Fatal(http.ListenAndServe(":5000", a.Router))
+	log.Printf("\nServer starting on port 5000")
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), a.Router))
 }
 
 func home(w http.ResponseWriter, r *http.Request) { // this is the home route
-    responses.JSON(w, http.StatusOK, "Welcome To RielzAPI")
+	responses.JSON(w, http.StatusOK, "Welcome To RielzAPI")
 }
